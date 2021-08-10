6fc101e80e3fff3cb427ad62a96b0caf72544c72
Nam Pham <phamnam3@hotmail.com>
2021-08-10 14:09:02 -0400

    Completed Explorer Mode

---
+++ queries.sql
@@ -0,0 +1,39 @@
+CREATE TABLE "Employees" (
+  "FullName"        TEXT,
+  "Salary"          INT,
+  "JobPosition"     TEXT,
+  "PhoneExtension"  INT,
+  "IsPartTime"      BOOLEAN
+);
+
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('Nam Pham', '1000', 'software developer', '727', 'false');
+
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('Kate Howard', '500', 'junior developer', '813', 'true');
+
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('Gordon Ramsay', '2000', 'cook', '900', 'false');
+
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('Lazy Larry', '100', 'customer service', '100', 'true');
+
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('Guy Fieri', '1500', 'cook', '727', 'false');
+
+SELECT * FROM "Employees";
+
+SELECT "FullName", "PhoneExtension" FROM "Employees" WHERE "IsPartTime" = false;
+
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+VALUES ('Jake Baker', '450', 'software developer', '727', 'true');
+
+UPDATE "Employees"
+SET "Salary" = 500
+WHERE "JobPosition" = 'cook';
+
+DELETE FROM "Employees"
+WHERE "FullName" = 'Lazy Larry';
+
+ALTER TABLE "Employees"
+ADD COLUMN "ParkingSpot" VARCHAR(10);
